24e6120114dd3788d062db82c9b70c4720781b0c
meeke <meekeboose@gmail.com>
2020-06-09 10:34:07 +0800

    css modules and global css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,23 +1,21 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-        <h4>
-          <a href="https://google.com" target="_blank">
-            Search Google
-          </a>
-        </h4>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </Layout>
     </>
   );
 }
